05b70a1f09f78d11ab9435f0d90e87c49b04f95b
Juno Brown <amarbrown@gmail.com>
2021-02-08 19:19:06 -0500

    added and model and controller

---
+++ Develop/controller/workoutController.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const Router = express.router();
+const Workout = require("../model/models/Workout");
+
+//require dependencies & create router instance
+const express = require("express");
+const Router = express.Router();
+const Workout = require("../models/Workout");
+
+// GET route for all workouts
+Router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then((allWorkouts) => {
+            res.json(allWorkouts);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// POST route for a new workout
+Router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then((newWorkout) => {
+            res.json(newWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// PUT route for creating a new workout or updating one
+Router.put("/api/workouts/:id", (req, res) => {
+    const id = req.params.id;
+    Workout.findByIdAndUpdate(
+        id,
+        {
+            $push: { exercises: req.body },
+        },
+        { new: true }
+    )
+        .then((updatedWorkout) => {
+            res.json(updatedWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// GET route to find last 7 workouts
+Router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(7)
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const workoutController = require("./Develop/controller/workoutController.js");
 const app = express();
 
 
 const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-  console.log("Mongoose successfully connected.");
+    console.log("Mongoose successfully connected.");
 });
 
 connection.on("error", (err) => {
-  console.log("Mongoose connection error: " + err);
+    console.log("Mongoose connection error: " + err);
 });
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(workoutController);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
